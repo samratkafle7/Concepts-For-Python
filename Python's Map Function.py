f88a858cefa78d4df1f5f987bceb00f4e8621b0a
Samrat Kafle <skafle3@leomail.tamuc.edu>
2023-01-19 13:21:32 -0600

    Usecase of Lambda and map function

--- Python's Map Function.py
+++ Python's Map Function.py
@@ -9,3 +9,7 @@ x = [21,23,34,34,54,54,6,23,12,34,45,46,56,67]
 # Convert map object into list and print.
 y = list(map(make_even_num, x))
 print(y)
+
+numbers = [1, 2, 3, 4, 5, 6]
+squared = map(lambda n:n**2, numbers)
+print(list(squared))
