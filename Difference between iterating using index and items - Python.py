af6f958e54a566514dfca7aa2ed0a1091584f172
Samrat Kafle <skafle3@leomail.tamuc.edu>
2023-01-20 12:59:17 -0600

    Example of iteration through the index and items.

---
+++ Difference between iterating using index and items - Python.py
@@ -0,0 +1,12 @@
+
+#Example of iteration through the index. 
+lst = ["red", "yellow", "green", "blue"]
+for i in range(len(lst)):
+    print(lst[i])
+
+#Example of iteration through the items. 
+
+print("----------------------------------------")    
+lst = ["red", "yellow", "green", "blue"]
+for i in lst:
+    print(i)
